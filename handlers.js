808bf9902bc23e4292167237b46d5e44530df251
Kaloyan-Dimitrov <kaloyan03092005@gmail.com>
2019-02-22 09:15:01 +0200

    Fixing merge issues

--- handlers.js
+++ handlers.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*jshint esversion: 6*/ 
 let mousePos = {
 	x: 0,
@@ -31,37 +30,3 @@ const handleTouchMove = (event) => {
     };
 };
 
-=======
-/*jshint esversion: 6*/ 
-let mousePos = {
-	x: 0,
-	y: 0
-};
-const handleWindowResize = () => {
-	HEIGHT = window.innerHeight;
-	WIDTH = window.innerWidth;
-	renderer.setSize(WIDTH, HEIGHT);
-	camera.aspect = WIDTH / HEIGHT;
-	camera.updateProjectionMatrix();
-};
-
-const handleMouseMove = (event) => {
-	const tx = -1 + (event.clientX / WIDTH) * 2;
-	const ty = -1 + (event.clientY / HEIGHT) * 2;
-	mousePos = {
-		x: tx,
-		y: -ty
-	};
-};
-
-const handleTouchMove = (event) => {
-	event.preventDefault();
-    const tx = -1 + (event.touches[0].clientX / WIDTH) * 2;
-    const ty = -1 + (event.touches[0].clientY / HEIGHT) * 2;
-    mousePos = {
-        x: tx,
-        y: -ty
-    };
-};
-
->>>>>>> a6f383b153d6e032897d0dac2eec540fd628da02
